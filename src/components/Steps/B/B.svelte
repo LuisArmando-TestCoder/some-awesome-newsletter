d598980c2dfd3bac856b419a820ca82a0753c226
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2025-03-16 01:34:46 -0600

    give overflow hidden away

--- src/components/Steps/B/B.svelte
+++ src/components/Steps/B/B.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <MarkdownText {canReveal}>  
-    Our **vertical AI agent-powered newsletter.** Just give it a ==website to feed from,== then **your lead,** and **the users you want to reach**—it's that easy. It **automatically detects** each user's ==country of residence.== The AI delivers **the most interesting article** from ==your feed==, **fully translated** into ==their language.==  
+    Our **vertical AI agent-powered newsletter.** Just give it a ==website to feed from,==then **your lead,** and **the users you want to reach**—it's that easy. It **automatically detects** each user's ==country of residence.== The AI delivers **the most interesting article** from ==your feed==, **fully translated** into ==their language.==  
 </MarkdownText>
 <div class="align-right">
     <MarkdownText {canReveal}>
-        *- Highlighted in an* --ADHD-friendly-- *way.*
+        *-* --Highlighted-- *in an* --ADHD-friendly-- *way.*
     </MarkdownText>
 </div>
 

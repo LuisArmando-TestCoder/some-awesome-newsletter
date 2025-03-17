9bc641d538d4c76afb4eef34cd03d2e13a3250f2
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2025-03-16 19:48:24 -0600

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // svelte.config.js
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 

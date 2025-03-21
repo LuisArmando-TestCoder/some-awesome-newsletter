78289ab824bab592c08ea364487920568c864c2b
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2025-03-21 00:44:38 -0600

    change

--- src/components/systems/steps/StepsTowardsPublish/stages/C_Welcome3.svelte
+++ src/components/systems/steps/StepsTowardsPublish/stages/C_Welcome3.svelte
@@ -9,11 +9,6 @@
   <MarkdownText {canReveal}>
     It **automatically detects** each user's ==country of residence.==The AI then delivers **the most interesting article** from ==your feed==, **fully translated** into ==their language.==
   </MarkdownText>
-  <div class="align-right">
-    <MarkdownText {canReveal}>
-      *-* --Highlighted-- *in an* --ADHD-friendly-- *way.*
-    </MarkdownText>
-  </div>
 
   <style lang="scss">
     .align-right {

778653106c6f95ae099b48b60e443fc62a33c57f
Luis Armando Oriens <luisarmando.murillobaltodano@gmail.com>
2025-06-23 11:13:49 -0600

    LAST STABLE VERSION

--- src/components/ThemeChanger/theme-store.ts
+++ src/components/ThemeChanger/theme-store.ts
@@ -1,11 +1,11 @@
 import { writable } from "svelte/store";
 
 const COLOR_1 = "#03a9f4";
-const COLOR_2 = "#f24e02";
+const COLOR_2 = "#03a9f4";
 
 export const themeIndex = writable<number>();
 export const foregroundColor = writable<string>(COLOR_1);
 export const foregroundColorSnapshot = COLOR_1;
 export const complementaryColor = writable<string>(COLOR_2);
 export const complementaryColorSnapshot = COLOR_2;
-export const isDarkTheme = writable<boolean>(true);
+export const isDarkTheme = writable<boolean>(false);
